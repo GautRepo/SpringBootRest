181693287c13e6eca521f9ae57d61187d5b3c140
GautRepo <gautam.kr0352@gmail.com>
2023-04-12 23:00:06 +0530

    Swagger UI Integration

---
+++ 35SBSwaggerUI/src/main/java/com/nt/controller/StudentController.java
@@ -0,0 +1,38 @@
+package com.nt.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest")
+public class StudentController {
+	
+	@GetMapping("/get")
+	public ResponseEntity<String> getMeth(){
+		return new ResponseEntity<String>("FromGet",HttpStatus.OK);
+	}
+	
+	@PostMapping("/post")
+	public ResponseEntity<String> postMeth(){
+		return new ResponseEntity<>("FromPost",HttpStatus.OK);
+	}
+	
+	@PutMapping("/put")
+	public ResponseEntity<String> putMeth(){
+		return new ResponseEntity<>("FromPut",HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/del")
+	public ResponseEntity<String> delMeth(){
+		return new ResponseEntity<>("FromDelete",HttpStatus.OK);
+	}
+	
+	
+
+}
